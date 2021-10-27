b102c78dd1e69e4e6cf13728dfd5fff021b0b7f1
Aurélien Stromboni <aurelien.stromboni@outlook.com>
2021-10-27 06:42:48 +0200

    Initial commit

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Discussions.Areas.Identity.Data;
+using Discussions.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Discussions.Areas.Identity.IdentityHostingStartup))]
+namespace Discussions.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<DiscussionsContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("DiscussionsContextConnection")));
+
+                services.AddIdentity<DiscussionsUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount=false)
+                    .AddEntityFrameworkStores<DiscussionsContext>()
+                    .AddDefaultUI()
+                    .AddDefaultTokenProviders();
+                services.AddControllersWithViews();
+            });
+        }
+    }
+}
